67759fb4889c6b9c3411e03e384a098a10ce3649
Nick Marsceau <nickmarsceau@gmail.com>
2022-01-04 19:57:05 -0500

    Replace State with Status in dcls command

--- docker-helper.ps1
+++ docker-helper.ps1
@@ -10,7 +10,7 @@ function dcls {
         [switch] $command
     );
     $allFlag = If ($all) {'--all'} Else {''};
-    $columns = [System.Collections.Generic.List[string]]@('{{.Names}}', '{{.ID}}', '{{.State}}');
+    $columns = [System.Collections.Generic.List[string]]@('{{.Names}}', '{{.ID}}', '{{.Status}}');
     If ($created) {$columns.Add('{{.CreatedAt}}');}
     If ($runningFor) {$columns.Add('{{.RunningFor}}');}
     If ($ports) {$columns.Add('{{.Ports}}');}
